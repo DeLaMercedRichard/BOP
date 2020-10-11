ba53e9464bfd65e60d1fd384cc3a907da5426a2a
DeLaMercedRichard <delamercedrichard@gmail.com>
2020-10-11 13:21:44 -0400

    TileMap Changes

--- BreathOfPumba/Assets/Enviroment/Scripts/RectangleRoom.cs
+++ BreathOfPumba/Assets/Enviroment/Scripts/RectangleRoom.cs
@@ -4,77 +4,130 @@ using UnityEngine;
 
 public class RectangleRoom : Room
 {
-    [ReadOnly]
-    List<Vector3Int> corners;
+
+    [Space(10)]
+    [Header("Sub-Properties")]
+    [SerializeField]
+    private List<Vector3Int> corners;
+
+    int radiusX, radiusY;
 
     // Start is called before the first frame update
     public override void Start()
     {
+      
         base.Start();
+        //Caching Variables
+        CacheVariables();
     }
 
     // Update is called once per frame
     public override void Update()
     {
         base.Update();
+        //DrawRoom();
     }
 
    
 
-    protected override void DrawRoom(int sizeX_, int sizeY_, Vector3Int position_)
+    protected override void DrawRoom()
     {
-        //Caching Variables
-        sizeX = sizeX_;
-        sizeY = sizeY_;
-        position = position_;
+        
         //Draw Floor
         DrawFloor();
         //Decide On Entrances 
 
         //Draw Walls
-        
+        DrawWalls();
 
     }
 
     protected override void DrawFloor()
     {
-        int radiusX, radiusY;
-        radiusX = sizeX / 2;
-        radiusY = sizeY / 2;
         //BoxFill(position, tile, start x, start y, end x , end y
-        //Draw Floor
+        floor.ClearAllTiles();
+        //***********SetBoundaries***********//
+        //Top Left
+        floor.SetTile(corners[3], floorTileAsset);
+        //Bottom Right
+        floor.SetTile(corners[1], floorTileAsset);
+
+
+        //Draws Floor
         floor.BoxFill(position, //origin position
-            wallTileAsset, //tile type
-            (position.x - radiusX), //start X
-            (position.y - radiusY), //start Y
-            (position.x + radiusX), //end X
-            (position.y + radiusY)); //end Y
+            floorTileAsset, //tile type
+            (corners[0].x), //start X
+            (corners[0].y), //start Y
+            (corners[2].x), //end X
+            (corners[2].y) //end Y
+            );
     }
 
     protected override void DrawWalls()
     {
-        //Caching Variables
-        Vector3Int temp;
-        temp = position;
-        temp.x = temp.x + sizeX;
-        //Bottom Left
-        corners.Add(position);
-        //Bottom Right
-        corners.Add(temp);
-        //Top Right
-        temp.y = temp.y + sizeY;
-        corners.Add(temp);
-        //Top Left
-        temp.x = temp.x - sizeX;
-        corners.Add(temp);
-
-        //TODO:
-        //Box Fill each edge 
-
-        //North
-        //West
-        //South
+        //North Wall
+        walls.BoxFill(position, //origin position
+         wallTileAsset, //tile type
+         (corners[3].x), //start X
+         (corners[3].y), //start Y
+         (corners[2].x), //end X
+         (corners[2].y) //end Y
+         );
         //East
-        throw new System.NotImplementedException();
+        walls.BoxFill(position, //origin position
+         wallTileAsset, //tile type
+         (corners[1].x), //start X
+         (corners[1].y), //start Y
+         (corners[2].x), //end X
+         (corners[2].y) //end Y
+         );
+        //South
+       walls.BoxFill(position, //origin position
+          wallTileAsset, //tile type
+          (corners[0].x), //start X
+          (corners[0].y), //start Y
+          (corners[1].x), //end X
+          (corners[1].y) //end Y
+          );
+        //West Wall Side
+        walls.BoxFill(position, //origin position
+           wallTileAsset, //tile type
+           (corners[0].x), //start X
+           (corners[0].y), //start Y
+           (corners[3].x), //end X
+           (corners[3].y) //end Y
+           );
+    }
+
+    private void CacheVariables()
+    {
+        radiusX = (int) (sizeX / 2);
+        radiusY = (int) (sizeY / 2);
+
+        Vector3Int temp;
+
+        //Bottom Left Corner
+        temp = position;
+        temp.x = temp.x - radiusX;
+        temp.y = temp.y - radiusY;
+        corners.Add(temp);
+
+        //Bottom Right
+        temp = position;
+        temp.x = temp.x + radiusX;
+        temp.y = temp.y - radiusY;
+        corners.Add(temp);
+
+        //Top Right
+        temp = position;
+        temp.x = temp.x + radiusX;
+        temp.y = temp.y + radiusY;
+        corners.Add(temp);
+
+        //Top Left
+        temp = position;
+        temp.x = temp.x - radiusX;
+        temp.y = temp.y + radiusY;
+        corners.Add(temp);
     }
 }
