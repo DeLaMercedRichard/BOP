8b140a181a882812467fc0fd5c770ece30a9ba20
Blawnode <blawnode@gmail.com>
2020-10-16 22:35:35 +0300

    Basic player animation complete

--- BreathOfPumba/Assets/Prefabs/Scripts/PlayerAction.cs
+++ BreathOfPumba/Assets/Prefabs/Scripts/PlayerAction.cs
@@ -17,8 +17,13 @@ public class PlayerAction : MonoBehaviour
     
     Coroutine FireingCorutine;
     [SerializeField] bool Gun = true;
-    
-   
+
+    Animator animator;  // By Blawnode
+
+    private void Start()
+    {
+        animator = transform.parent.GetComponent<Animator>();  // By Blawnode
+    }
 
     // Update is called once per frame
     void Update()
@@ -43,7 +48,11 @@ public class PlayerAction : MonoBehaviour
         Vector2 MousePosition = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
         float angle = Weaponangle(WeaponPosition, MousePosition);
         transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
-    } 
+
+        Vector2 angleVector = (Vector2)(Quaternion.Euler(0, 0, angle) * Vector2.right);  // By Blawnode, https://answers.unity.com/questions/823090/equivalent-of-degree-to-vector2-in-unity.html
+        if (System.Math.Abs(-angleVector.x) > 0.01) animator.SetFloat("Horizontal", -angleVector.x);  // By Blawnode
+        if (System.Math.Abs(-angleVector.y) > 0.01) animator.SetFloat("Vertical", -angleVector.y);  // By Blawnode
+    }
 
     float Weaponangle(Vector3 a, Vector3 b)
     {
