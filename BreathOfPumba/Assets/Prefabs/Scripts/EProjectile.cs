829e02eef4d9e781a6bacd6fb1a25a960f7ed1ce
Blawnode <blawnode@gmail.com>
2020-10-17 13:18:55 +0300

    Turret animations complete!

--- BreathOfPumba/Assets/Prefabs/Scripts/EProjectile.cs
+++ BreathOfPumba/Assets/Prefabs/Scripts/EProjectile.cs
@@ -6,6 +6,8 @@ public class EProjectile : MonoBehaviour
 {
     GameObject target;
     Rigidbody2D ProjectileRB;
+    public GameObject spriteObject;  // By Blawnode
+
     [SerializeField] float ProjectileSpeed = 5;
     void Start()
     {
@@ -13,6 +15,8 @@ public class EProjectile : MonoBehaviour
         target = GameObject.FindGameObjectWithTag("Player");
         Vector2 Direction = (target.transform.position - transform.position).normalized * ProjectileSpeed;
         ProjectileRB.velocity = new Vector2(Direction.x, Direction.y);
+        var angle = Mathf.Atan2(Direction.y, Direction.x) * Mathf.Rad2Deg;  // By Blawnode, https://answers.unity.com/questions/654222/make-sprite-look-at-vector2-in-unity-2d-1.html
+        spriteObject.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);  // By Blawnode, https://answers.unity.com/questions/654222/make-sprite-look-at-vector2-in-unity-2d-1.html
         Destroy(this.gameObject, 2);
     }
 }
