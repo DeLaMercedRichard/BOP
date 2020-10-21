77b02738b780558cf86bbfeebd3cbd7473e99874
DeLaMercedRichard <delamercedrichard@gmail.com>
2020-10-21 12:19:29 -0400

    Audio Control

--- BreathOfPumba/Assets/Prefabs/Scripts/Managers/GameManager.cs
+++ BreathOfPumba/Assets/Prefabs/Scripts/Managers/GameManager.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    AudioControl musicPlayer;
+  
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Play the Menu Music At Start (assuming menu is the first scene that loads up)
+        musicPlayer.PlayTrack(AudioControl.TrackType.Menu);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    public void ChangeTrack(AudioControl.TrackType trackType)
+    {
+        musicPlayer.PlayTrack(trackType);
     }
 }
